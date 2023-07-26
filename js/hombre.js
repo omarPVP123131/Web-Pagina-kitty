d7b414a8c93bfee1cb5bdc3c0971df9218a7a7ce
omarPVP123131 <palomaresomar691@gmail.com>
2023-07-26 01:17:46 -0600

    a

--- js/hombre.js
+++ js/hombre.js
@@ -2,6 +2,10 @@
 const cartIcon = document.querySelector(".cart-icon");
 const cart = document.querySelector(".cart");
 const checkoutBtn = document.querySelector(".checkout"); // Obtener el botón "Finalizar compra"
+const seeMoreButton = document.querySelector(".see-more"); // Obtener el botón "Ver más"
+
+// Ocultar el carrito al inicio
+cart.style.display = "none";
 
 // Evento para mostrar/ocultar el carrito al hacer clic en el ícono
 cartIcon.addEventListener("click", () => {
@@ -10,9 +14,6 @@ cartIcon.addEventListener("click", () => {
 
 // Array para almacenar los productos agregados al carrito
 const cartProducts = [];
-
-const addToCartButtons = document.querySelectorAll(".add-to-cart");
-const cartItemsList = document.querySelector(".cart-items");
 
 // Evento para agregar productos al carrito al hacer clic en el botón "Agregar al carrito"
 addToCartButtons.forEach((button, index) => {
@@ -43,15 +44,12 @@ addToCartButtons.forEach((button, index) => {
 
         // Si hay más de 4 productos en el carrito, mostrar el botón "Ver más"
         if (cartItemsList.children.length > 4) {
-            const seeMoreButton = document.querySelector(".see-more");
             seeMoreButton.style.display = "block";
         }
     });
 });
 
-
 // Evento para mostrar/ocultar más productos al hacer clic en el botón "Ver más"
-const seeMoreButton = document.querySelector(".see-more");
 seeMoreButton.addEventListener("click", () => {
     // Mostrar 4 productos ocultos en el carrito
     const hiddenItems = document.querySelectorAll(".cart-items li.hidden");
@@ -67,10 +65,11 @@ seeMoreButton.addEventListener("click", () => {
 
 // Evento para finalizar la compra y generar el PDF
 checkoutBtn.addEventListener("click", () => {
-    console.log("evento1")
+    console.log("evento1");
     // Generar el PDF con los productos del carrito
     generatePDF(cartProducts);
 });
+
 
 // Función para generar el PDF con los productos del carrito
 function generatePDF(products) {
